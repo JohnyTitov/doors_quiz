4d4162395070a6799ff5b4e5428eec31de95bb31
Johny <jeka-titov@bk.ru>
2021-08-12 17:18:03 +0300

    smal fontend change

--- doors_quiz/urls.py
+++ doors_quiz/urls.py
@@ -4,9 +4,9 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('test/', include('pages.urls')),
     path('', include('dveri.urls')),
-    path('site/admin/', admin.site.urls),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
