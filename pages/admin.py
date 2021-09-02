7d2667242129e02a8ae8cd2bb5859a71dd8b029f
Johny <jeka-titov@bk.ru>
2021-09-02 12:30:44 +0300

    one shop to many clients

--- pages/admin.py
+++ pages/admin.py
@@ -12,6 +12,7 @@ class AdminClientChoice(admin.ModelAdmin):
     list_display = ('shop', 'phone', 'feedback', 'type_door', 'color_door')
     list_display_links = ('shop', 'phone', 'feedback', 'type_door', 'color_door')
     search_fields = ('shop', 'phone', 'feedback', 'type_door', 'color_door')
+    list_filter = ('shop', 'feedback', 'type_door', 'color_door')
 
 
 admin.site.register(PageShop, AdminPageShop)
